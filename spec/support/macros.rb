37eef93c3eaa578761f1e51aaa9ebb8c26aa0b7f
long.yang <long.yang@starlight-sms.com>
2016-03-24 11:36:15 +0800

    feature test

--- spec/support/macros.rb
+++ spec/support/macros.rb
@@ -5,3 +5,11 @@ end
 def clear_session
   session[:user_id] = nil
 end
+
+def sign_in(a_user = nil)
+  user = a_user || Fabricate(:user)
+  visit sign_in_path
+  fill_in "user[email]", with: user.email
+  fill_in "user[password]", with: user.password
+  click_button "Sign in"
+end
